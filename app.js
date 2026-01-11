628ffb4fe3266f1eb5f802b0304ee2b3568bff99
sania shahzad <saniashahzad768@gmail.com>
2026-01-11 11:40:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+//***********Chapter#1*************//
+     
+      //-------Question#1---------//
+     //alert("welcome to my website ");  
+
+      //-------Question#2--------//
+    //alert("Error! please enter a valid password.  ")      
+
+      //-------Question#3--------//
+    //alert("welcome to js land....\n happy coding ! ")
+
+       //-------Question#4--------//
+      //alert("welcome to js land...")
+    //alert("happy coding ! \n prevent this page from creating additional dialogs")
+
+       //-------Question#5--------//
+    //console.log("hello...i can run js through my web browers console ")
+
+       
+  //========//===end===//========//
+      
+      
+//***********Chapter#2*************//
+     
+      //-------Question#1---------//
+     //var username ;
+
+      //-------Question#2--------//
+     //var myname = "saniashahzad" ;  
+
+      //-------Question#3--------//
+     //var message ;   // a
+     //message ="hello world" ;    //b
+     //alert( message);     //c
+
+       //-------Question#4--------//
+     //var studentName =" saniashahzad ";
+     //var age = "15";
+     //var course ="computer science";
+     //alert("student Name: " + studentName)
+     //alert("Age: " + age)
+     //alert("Course: " + course)
+    
+       //-------Question#5--------//
+     //var alertMessage = "Welcome to JavaScript!";
+     //alert(alertMessage);
+
+    
+      //-------Question#6--------//
+     //var email = "saniashahzad768@gmail.com";
+     //alert("My email address is " + email);
+
+       //-------Question#7--------//
+     //var book = "A smarter way to learn JavaScript";
+     //alert("I am trying to learn from the book " + book);
+
+
+       //-------Question#8--------//
+     //alert("I can write HTML content through JavaScript");
+
+       
+      //-------Question#9--------//
+     //var message2 = "Yah! I can write HTML content through JavaScript";
+     //alert(message2);
+     //document.write("<h1> YEAH I CAN WRITE HTML CONTENT WITH JAVA SCRIPT</h1>")
+
+
+    
+  //========//===end===//========//
+      
+
+  //***********Chapter#3*************//
+     
+      //-------Question#1---------//
+    //var age =15
+   // alert( "i am" + age + "years old") 
+    
+      //-------Question#2--------//
+    //var visitCount = 14
+   // document.write (" you visited this site " + visitCount +  "time.<br>")      
+
+      //-------Question#3--------//
+    //var brithday =2010 .
+     //document.write(" my brith year is " + brithday + "<br>") 
+     //document.write ("data type of my  declared variable is number .<br>") ;
+    
+       //-------Question#4--------//
+    //var Visitor_Name = "Sania" ; //a
+    //var product_title = " cloth " ; //b
+    //var quantity = 5 ; //c
+    //document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+      
+  //========//===end===//========//
+      
+
+  //***********Chapter#4*************//
+     
+      //-------Question#1---------//
+   //var name, age, city; 
+
+      //-------Question#2--------//
+
+ 
+
+      //-------Question#3--------//
+//document.write("<hr><h1>Rules for naming JS variables</h1><br><hr>");
+//document.write("Variable names can only contain letters, numbers, $ and _ . For example $my_1stVariable<br><br>");
+//document.write("Variables must begin with a letter, $ or _ . For example $name, _name or name<br><br>");
+//document.write("Variable names are case sensitive<br><br>");
+//document.write("Variable names should not be JS keywords");
+
+
+    
+  //========//===end===//========//
+      
